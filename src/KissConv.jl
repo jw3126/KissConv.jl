352d9a9cd8bbd711f3047737b145ac770d2f9445
Jan Weidner <jw3126@gmail.com>
2020-05-18 15:21:57 +0200

    fix

--- src/KissConv.jl
+++ src/KissConv.jl
@@ -1,5 +1,56 @@
 module KissConv
 
-# Write your package code here.
+using OffsetArrays
+export conv
 
+struct Inner end
+struct Outer end
+
+function conv_axes(arr1, arr2, ::Inner)
+    if ndims(arr1) != ndims(arr2)
+        throw(DimensionMismatch("Cannot convolve arrays of different dimensions."))
+    end
+    axs = map(axes(arr1), axes(arr2)) do r1, r2
+        i_start = first(r1) +  last(r2)
+        i_stop  = last(r1)  + first(r2)
+        i_start:i_stop
+    end
+    return axs
 end
+
+function conv_axes(arr1, arr2, ::Outer)
+    if ndims(arr1) != ndims(arr2)
+        throw(DimensionMismatch("Cannot convolve arrays of different dimensions."))
+    end
+    axs = map(axes(arr1), axes(arr2)) do r1, r2
+        i_start = first(r1) + first(r2)
+        i_stop  = last(r1)  + last(r2)
+        i_start:i_stop
+    end
+    return axs
+end
+
+function conv_out(arr1, arr2, mode)
+    T = promote_type(eltype(arr1), eltype(arr2))
+    axs = conv_axes(arr1, arr2, mode)
+    return similar(arr1, T, axs)
+end
+
+function conv!(out, arr1, arr2, ::Inner)
+    z = zero(eltype(out))
+    @inbounds for I in CartesianIndices(out)
+        out[I] = z
+        @simd for J in CartesianIndices(arr2)
+            IJ = CartesianIndex(map(-, Tuple(I), Tuple(J)))
+            out[I] += arr1[IJ] * arr2[J]
+        end
+    end
+    return out
+end
+
+function conv(arr1, arr2, mode=Inner())
+    out = conv_out(arr1, arr2, mode)
+    return conv!(out, arr1, arr2, mode)
+end
+
+end#module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,25 @@
-using KissConv
 using Test
+import NNlib
+import KissConv
 
-@testset "KissConv.jl" begin
-    # Write your tests here.
+to_whcn(arr) = reshape(arr, (size(arr)..., 1, 1))
+
+@testset "Against NNlib" begin
+    for item in [
+            (arr1=randn(10), arr2=randn(2)),
+            (arr1=randn(Float32, 10), arr2=randn(Float32, 2)),
+            (arr1=randn(Float32, 2,3), arr2=randn(Float32, 2,1)),
+            (arr1=rand(1:10, 2,3), arr2=randn(Float32, 2,1)),
+            (arr1=rand(1:100, 2,3,4), arr2=rand(1:100, 2,1, 3)),
+        ]
+
+        res_nn = NNlib.conv(
+            to_whcn(item.arr1),
+            to_whcn(item.arr2),
+        )
+
+        res_kiss = @inferred KissConv.conv(item.arr1, item.arr2)
+        @test eltype(res_kiss) == eltype(res_nn)
+        @test to_whcn(res_kiss) ≈ res_nn
+    end
 end
